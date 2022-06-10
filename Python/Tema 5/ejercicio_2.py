bb6690388b929e3a09ce6e22e33df1ba663eb42d
Jesica Lerner <102632133+JessLerner@users.noreply.github.com>
2022-06-10 20:50:15 -0300

    Update ejercicio_2.py
    el codigo al hacerlo correr daba multiples respuestas..

--- Python/Tema 5/ejercicio_2.py
+++ Python/Tema 5/ejercicio_2.py
@@ -1,15 +1,25 @@
 def numeroprimo():
-  numero: int = int(input('Introduce un número entero: '))
+	numero= int(input('Introduce un número entero: '))
+	primo=False
 
-  if numero > 1:
-    # Buscamos los factores de número
-    for i in range(2,int(numero)):
-        if (int(numero) % i) == 0:
-            print(f"Lo siento, el número {numero} NO ES PRIMO. Es divisible entre {i}")
-            break
-        else:
-            print(f"¡ENHORABUENA!, el número {numero} ES PRIMO")
-  else:
-    print(f"Lo siento, el número {numero} NO ES PRIMO. Los números primos son mayores que 1")
+	if numero > 1
+		for i in range(2,numero):
+			if numero% i== 0 :
+				print("a")
+				primo=False
+				break
+			else:
+				primo=True
+		return primo
+	else:
+		return (f"Lo siento, el número {numero} NO ES PRIMO. Los números primos son mayores que 1")
+  
+  
+respuesta=numeroprimo()
 
-print({numeroprimo()})
+if respuesta==True:
+	print("El numero es Primo")
+elif respuesta==False:
+	print("El numero no es primo")
+else:
+	print(respuesta)
